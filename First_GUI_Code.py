4c3eeaf77d2e9307e0c3cc79e6219d52201d4ac6
Abdallah-ship-it-oss <fighter7123@gmail.com>
2026-09-26 18:15:42 +0300

    Update First_GUI_Code.py

--- First_GUI_Code.py
+++ First_GUI_Code.py
@@ -7,74 +7,40 @@
 #
 #============================================
 
-import random
+import tkinter as tk
+window = tk.TK ()
+window.title("mein projekt 🤍")
+window.geometry("400×500")
+
+# Abdallah'z main manu 📙
+
+title = tk.Label (window ,text ="Main Manu" ,front =("Arial" ,18 ,"blod"))
+title.pack(pady = 20)
+
+#Buttons
+but1 = tk.Button(window ,text ="1 - Add new German word. " ,width = 30 )
+but1.pack(pady = 5)
 
 
-word_book = {}
-Workout = []
+but2 = tk.Button(window ,text ="2 - Add new Workout. " ,width = 30 )
+but2.pack(pady = 5)
 
-def main_menu () : 
-    while True :
-        print("="* 35 )
-        print("|_____Main_Menu_____|")
-        print("="* 35 )
-        print("1 - add new german word")
-        print("2 - add new Workout")
-        print("3 - German words test ")
-        print("4 - show all data ")
-        print("5 - close ")
-        print("="* 35 )
-    
-        
-        choice = input("chose a Number : ")
-        if choice =="1" :
-            german_word = input("the word with German :")
-            arabic_word = input("the word with Arabic :")
-            word_book[german_word] = arabic_word
-            with open ("word_book.txt" ,"a" ,encoding = "utf-8") as file :
-                file.write(f"{german_word}:{arabic_word}")
-                
-            print("the word has been saved . " )
-            
-            
-            
-        elif choice == "2" :
-            new_Workout = input("what's your new Workout : ")
-            Workout.append(new_Workout)
-            with open ("Workout.txt" , "a" , encoding = "utf-8") as file :
-                file.write(f"{new_Workout}")
-            print("the Workout has been saved ! ")
-            
-            
-        elif choice == "3" :
-            print("we will ad this soon ")
-            
-            
-        elif choice == "4" :
-            print("=" * 35 )
-            if not word_book :
-                print("there's no words yet ! ")
-            else :   
-                print(word_book)
-            print("=" * 35 )
-            print("☆" * 35 )
-            print("=" * 35 )
-            if not Workout :
-                print ("there's no Workout yet! ")
-            else :
-                print(Workout)
-            print("=" * 35 )
-            
-            
-        elif choice == "5" :
-            print("good bye .")
-            break
-            
-            
-        else :
-            print("wrong choice !")
- 
-            
-main_menu()           
-                    
-    
+
+but3 = tk.Button(window ,text ="3 - German words test. " ,width = 30 )
+but3.pack(pady = 5)
+
+
+but4 = tk.Button(window ,text ="4 - show all data. " ,width = 30 )
+but4.pack(pady = 5)
+
+
+but5 = tk.Button(window, text ="5 - Close App . " ,width = 30 ,command = window.destroy)
+but5.pack(pady = 5)
+
+
+
+
+
+
+
+window.mainloop()
